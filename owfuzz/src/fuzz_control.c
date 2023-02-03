a0bf576299883bbba5f522c859d1b0f0937d2e65
Noam Rathaus <rathaus@gmail.com>
2023-02-03 18:33:15 +0200

    Uncomment the log level explain

--- owfuzz/src/fuzz_control.c
+++ owfuzz/src/fuzz_control.c
@@ -141,7 +141,7 @@ uint8_t all_frames[] =
 void usage_help(char *name)
 {
 	printf("owfuzz usage:\n"
-		   "\texample: sudo ./owfuzz -i wlan0 -m ap -c [channel] -t [target-mac] -b [ap-mac] -s [ap-mac] -T 2 -A WPA2_PSK_TKIP_AES -I [targe-ip]\n"
+		   "\texample: sudo ./owfuzz -i wlan0 -m ap -c [channel] -t [target-mac] -b [ap-mac] -s [ap-mac] -T %d -A WPA2_PSK_TKIP_AES -I [targe-ip]\n"
 		   "\t-i [interface]\n"
 		   "\t   Interface to use.\n"
 		   "\t-m [ap/sta]\n"
@@ -162,12 +162,12 @@ void usage_help(char *name)
 		   "\t   Fuzzer's (source) Mac address.\n"
 		   "\t-T [test type]\n"
 		   "\t   Test type, default %d, %d: Poc test, %d: interactive test, %d: frames test, %d: interactive & frames test\n"
-		   //		"\t-l [log level]\n"
-		   //		"\t   Log level, 8:DEBUG, 7:INFO, 6:NOTICE, 5:WARN, 4:ERR, 3:CRIT, 2:ALERT, 1:EMERG, 0:STDERR\n"
+			"\t-l [log level]\n"
+			"\t   Log level, 8:DEBUG, 7:INFO, 6:NOTICE, 5:WARN, 4:ERR, 3:CRIT, 2:ALERT, 1:EMERG, 0:STDERR\n"
 		   "\t-f [log file]\n"
 		   "\t   Log file path\n"
 		   "\t-h\n"
-		   "\t   Help.\n", TEST_INTERACTIVE, TEST_POC, TEST_INTERACTIVE, TEST_FRAME, TEST_INTERACTIVE_FRAME);
+		   "\t   Help.\n", TEST_FRAME, TEST_INTERACTIVE, TEST_POC, TEST_INTERACTIVE, TEST_FRAME, TEST_INTERACTIVE_FRAME);
 }
 
 void *test_bad_frame(void *param)
