5f81e3a73114762c886613aba8cd66e8233b8e31
Noam Rathaus <rathaus@gmail.com>
2023-02-04 10:14:06 +0200

    Init value

--- owfuzz/src/frames/management/action_no_ack.c
+++ owfuzz/src/frames/management/action_no_ack.c
@@ -30,7 +30,7 @@ void load_action_no_ack_state()
 
 struct packet create_action_no_ack(struct ether_addr bssid, struct ether_addr smac, struct ether_addr dmac, char adhoc, struct packet *recv_pkt)
 {
-    struct ieee_hdr *hdr;
+    struct ieee_hdr *hdr = NULL;
     struct packet pkt = {0};
 
     pkt = create_action(bssid, smac, dmac, 0, recv_pkt);
